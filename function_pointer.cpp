276aa9f5960530467c254cbbdd2239dd857ee686
Mahesh Bapatu <153306023+maheshrbapatu@users.noreply.github.com>
2026-02-17 21:26:39 -0500

    Update function_pointer.cpp

--- function_pointer.cpp
+++ function_pointer.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
+#include <functional>
 using namespace std;
 
-// Normal function
-int add(int a, int b) {
-    return a + b;
+class Calculator {
+public:
+    int add(int a, int b) const { return a + b; }
+    int mul(int a, int b) const { return a * b; }
+};
+
+/************ (2) Template helper: generic caller for member function pointers ************/
+template <typename T, typename Ret, typename... Args>
+Ret call_member(const T& obj, Ret (T::*mfp)(Args...) const, Args... args) {
+    return (obj.*mfp)(args...);
 }
 
 int main() {
-    // Function pointer declaration
-    int (*fp)(int, int);
+    Calculator c;
 
-    // Assign function address to pointer
-    fp = add;
+    /************ (1) Member function pointer ************/
+    int (Calculator::*mfp_add)(int, int) const = &Calculator::add;
+    cout << "member function pointer: " << (c.*mfp_add)(3, 4) << "\n";
 
-    // Call using normal function
-    int r1 = add(2, 3);
+    /************ (2) Member function pointer used via templates ************/
+    cout << "template caller:         " << call_member(c, &Calculator::mul, 3, 4) << "\n";
 
-    // Call using function pointer
-    int r2 = fp(2, 3);
+    /************ (3) std::function wrapping a class function (via lambda capture) ************/
+    std::function<int(int,int)> sf = [&c](int a, int b) {
+        return c.add(a, b);
+    };
+    cout << "std::function:           " << sf(10, 20) << "\n";
 
-    // 3. std::function
-    std::function<int(int,int)> f = add;
-    int r3 = f(2, 3);
+    /************ (4) std::bind binding object + member function ************/
+    auto bound = std::bind(&Calculator::mul, &c,
+                           std::placeholders::_1,
+                           std::placeholders::_2);
+    cout << "std::bind:               " << bound(6, 7) << "\n";
 
-    cout << "Normal call: " << r1 << "\n";
-    cout << "Pointer call: " << r2 << "\n";
-    cout << "std::fucntion call: " << r3 << "\n";
+    return 0;
 }
