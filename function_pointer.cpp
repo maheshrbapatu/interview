eafdaf8aeee92d56a7b734ae9b1ee25e369ed2f3
Mahesh Bapatu <153306023+maheshrbapatu@users.noreply.github.com>
2026-02-17 21:33:36 -0500

    Update function_pointer.cpp

--- function_pointer.cpp
+++ function_pointer.cpp
@@ -9,37 +9,38 @@ public:
 };
 
 /******** TEMPLATE GENERIC CALLER USING std::invoke ********/
-template <typename Callable, typename Obj, typename... Args>
-auto call_any(Callable&& fn, Obj&& obj, Args&&... args) {
+template <typename Callable, typename... Args>
+decltype(auto) call_any(Callable&& fn, Args&&... args) {
+    // invoke is needed here because "fn" could be:
+    //  - a member function pointer (needs special call syntax)
+    //  - a normal function/lambda/functor (callable via operator())
     return std::invoke(std::forward<Callable>(fn),
-                       std::forward<Obj>(obj),
                        std::forward<Args>(args)...);
 }
 
 int main() {
     Calculator c;
 
-    /******** 1. MEMBER FUNCTION POINTER ********/
+    /******** 1) MEMBER FUNCTION POINTER ********/
+    // invoke IS needed here (member function pointers can't be called like a normal function)
     auto mfp = &Calculator::add;
-    cout << "member ptr + invoke: "
-         << std::invoke(mfp, c, 3, 4) << "\n";
+    cout << "member ptr + invoke: " << std::invoke(mfp, c, 3, 4) << "\n";
 
-    /******** 2. TEMPLATE + INVOKE ********/
-    cout << "template invoke:    "
-         << call_any(&Calculator::mul, c, 5, 6) << "\n";
+    /******** 2) TEMPLATE (generic) ********/
+    // invoke IS needed inside call_any for generic callability
+    cout << "template invoke:     " << call_any(&Calculator::mul, c, 5, 6) << "\n";
 
-    /******** 3. std::function ********/
-    std::function<int(int,int)> sf =
-        [&c](int a, int b) { return c.add(a, b); };
+    /******** 3) std::function ********/
+    // invoke is NOT needed here; std::function is directly callable
+    std::function<int(int,int)> sf = [&c](int a, int b) { return c.add(a, b); };
+    cout << "std::function:       " << sf(7, 8) << "\n";
 
-    cout << "std::function:      "
-         << std::invoke(sf, 7, 8) << "\n";
-
-    /******** 4. std::bind ********/
+    /******** 4) std::bind ********/
+    // bind is ONLY "needed" if you want to bind the object now and call later like a free function
     auto bound = std::bind(&Calculator::mul, &c,
                            std::placeholders::_1,
                            std::placeholders::_2);
 
-    cout << "bind + invoke:      "
-         << std::invoke(bound, 9, 10) << "\n";
+    // invoke is NOT needed here either; bind returns a callable object
+    cout << "bind (callable):     " << bound(9, 10) << "\n";
 }
