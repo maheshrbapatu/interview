2a5f030906333491d6e19a06d64fa922e57a106e
Mahesh Bapatu <153306023+maheshrbapatu@users.noreply.github.com>
2026-02-17 22:23:56 -0500

    Update specialization.cc

--- templates/specialization.cc
+++ templates/specialization.cc
@@ -6,9 +6,29 @@ struct Printer {
     static void print(const T& x) { std::cout << x << "\n"; }
 };
 
+// full specialization
 template <>
 struct Printer<std::string> {
     static void print(const std::string& s) { std::cout << '"' << s << "\"\n"; }
+};
+
+
+// Primary template (generic)
+template<typename T>
+struct Box {
+    static constexpr const char* tag = "primary: Box<T>";
+};
+
+// Partial specialization for pointers
+template<typename T>
+struct Box<T*> {
+    static constexpr const char* tag = "partial: Box<T*>";
+};
+
+// Partial specialization for const types
+template<typename T>
+struct Box<const T> {
+    static constexpr const char* tag = "partial: Box<const T>";
 };
 
 int main() {
