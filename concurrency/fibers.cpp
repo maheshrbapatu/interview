d59e1958c10c449f3a5f47bdce6fa2a55ebea78f
Mahesh Bapatu <153306023+maheshrbapatu@users.noreply.github.com>
2026-02-19 17:38:24 -0500

    Update fibers.cpp

--- concurrency/fibers.cpp
+++ concurrency/fibers.cpp
@@ -1,11 +1,20 @@
-#include <future>
+#include <boost/fiber/all.hpp>
 #include <iostream>
 
-int work() {
-    return 10;
+void fiber_fn(const char* name) {
+    for (int i = 0; i < 5; i++) {
+        std::cout << name << " step " << i << "\n";
+        boost::this_fiber::yield();   // cooperative switch to another fiber
+    }
 }
 
 int main() {
-    auto f = std::async(work);
-    std::cout << f.get();  // waits + returns value
+    boost::fibers::fiber f1(fiber_fn, "fiber-1");
+    boost::fibers::fiber f2(fiber_fn, "fiber-2");
+
+    f1.join();
+    f2.join();
+
+    std::cout << "done\n";
+    return 0;
 }
